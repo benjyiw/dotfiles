dba41797ec56f6abe8ddbd21b2f723ae61f023e0
benjyiw <ben@wese.io>
2022-01-17 21:19:42 -0700

    completion for kubectl

---
+++ .oh-my-zsh-custom/completions.zsh
@@ -0,0 +1,13 @@
+# kubectl, copied from the kubectl plugin.
+# not using that plugin though because i don't want the aliases.
+if (( $+commands[kubectl] )); then
+    __KUBECTL_COMPLETION_FILE="${ZSH_CACHE_DIR}/kubectl_completion"
+
+    if [[ ! -f $__KUBECTL_COMPLETION_FILE || ! -s $__KUBECTL_COMPLETION_FILE ]]; then
+        kubectl completion zsh >! $__KUBECTL_COMPLETION_FILE
+    fi
+
+    [[ -f $__KUBECTL_COMPLETION_FILE ]] && source $__KUBECTL_COMPLETION_FILE
+
+    unset __KUBECTL_COMPLETION_FILE
+fi

7aa34ef9e72b2164f2842717484046ffdb1be027
benjyiw <ben@wese.io>
2022-09-28 09:41:43 -0600

    completions for helm and k9s

--- .oh-my-zsh-custom/completions.zsh
+++ .oh-my-zsh-custom/completions.zsh
@@ -11,3 +11,29 @@ if (( $+commands[kubectl] )); then
 
     unset __KUBECTL_COMPLETION_FILE
 fi
+
+# helm
+if (( $+commands[helm] )); then
+    __HELM_COMPLETION_FILE="${ZSH_CACHE_DIR}/helm_completion"
+
+    if [[ ! -f $__HELM_COMPLETION_FILE || ! -s $__HELM_COMPLETION_FILE ]]; then
+        helm completion zsh >! $__HELM_COMPLETION_FILE
+    fi
+
+    [[ -f $__HELM_COMPLETION_FILE ]] && source $__HELM_COMPLETION_FILE
+
+    unset __HELM_COMPLETION_FILE
+fi
+
+# k9s
+if (( $+commands[k9s] )); then
+    __K9S_COMPLETION_FILE="${ZSH_CACHE_DIR}/k9s_completion"
+
+    if [[ ! -f $__K9S_COMPLETION_FILE || ! -s $__K9S_COMPLETION_FILE ]]; then
+        k9s completion zsh >! $__K9S_COMPLETION_FILE
+    fi
+
+    [[ -f $__K9S_COMPLETION_FILE ]] && source $__K9S_COMPLETION_FILE
+
+    unset __K9S_COMPLETION_FILE
+fi

6ea5c2400adea9d974ff0cbb05c7f56adc9efb7f
benjyiw <ben@wese.io>
2023-04-24 09:59:35 -0600

    center search in vim

--- .config/nvim/lua/remaps.lua
+++ .config/nvim/lua/remaps.lua
@@ -16,3 +16,6 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 -- move highlighted text up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- center search
+vim.keymap.set("n", "n", "nzzzv", opts)

40f04bee634d72b9b9f3d59574ff8bd7722f5d3b
benjyiw <ben@wese.io>
2023-12-26 11:12:46 -0700

    mapping for git blame, terraform commentary config

--- .config/nvim/after/plugin/fugitive.lua
+++ .config/nvim/after/plugin/fugitive.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<leader>gs", ":G<CR>",     { noremap = true })
 vim.keymap.set("n", "<leader>G",  ":tab G<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>gb", ":G blame<CR>", { noremap = true })
